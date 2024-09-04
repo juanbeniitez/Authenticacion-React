fb6f0ef7bc9fe7219558a2f421dc330fbd3a5698
juanbeniitez <tjuanbenitez@gmail.com>
2024-09-04 15:49:02 +0200

    Update

--- auth-backend/src/config/mailtrap.js
+++ auth-backend/src/config/mailtrap.js
@@ -1,7 +1,7 @@
 import { MailtrapClient } from 'mailtrap';
 
 const mailtrapClient = new MailtrapClient({
-  token: `f5ae43768c6ab0d0d98cefd6c4775eb6`,
+  token: `${process.env.MAILTRAP_TOKEN}`,
 });
 
 const sender = {
