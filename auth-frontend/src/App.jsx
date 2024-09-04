16f617c6bdf12ec83526ca74153cb36961dedcdc
juanbeniitez <tjuanbenitez@gmail.com>
2024-09-04 19:30:21 +0200

    Signup page created

--- auth-frontend/src/App.jsx
+++ auth-frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import FloatingShape from '@components/FloatingShape'
+import SignUp from '@pages/SignUp'
+import LoginPage from '@pages/LoginPage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
  return(
@@ -7,6 +10,13 @@ function App() {
     <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0}/>
     <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5}/>
     <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2}/>
+
+    <Routes>
+      <Route path='/' element={"Home"}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+    </Routes>
+
   </div>
  )
 }
--- auth-frontend/vite.config.js
+++ auth-frontend/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@context': path.resolve(__dirname, 'src/context'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
     },
   },
 })
